78a25751dbd8582c233064765cced1eb6fe2a731
vnyoon <vnyoon@163.com>
2024-06-27 20:12:41 +0800

    refactor(Sidebar)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,63 +10,56 @@ import {
 } from "react-icons/fa";
 
 const Sidebar = memo(() => {
+  const sidebarList = [
+    { 
+      id: crypto.randomUUID(), 
+      name: '热门榜', 
+      path: '/hot-books', 
+      icon: <FaFire />
+    },
+    { 
+      id: crypto.randomUUID(), 
+      name: '新品发布', 
+      path: '/new-releases', 
+      icon: <FaFolderPlus /> 
+    },
+    { 
+      id: crypto.randomUUID(), 
+      name: '即将推出', 
+      path: '/coming-soon', 
+      icon: <FaCalendarAlt /> 
+    },
+    { 
+      id: crypto.randomUUID(), 
+      name: '收藏夹', 
+      path: '/favorites', 
+      icon: <FaRegHeart /> 
+    },
+    { 
+      id: crypto.randomUUID(), 
+      name: '稍后观看', 
+      path: '/watch-later', 
+      icon: <FaRegClock /> 
+    }
+  ];
+
   return (
-    <aside className="bg-orange-500">
+    <aside>
       <ul className="space-y-2">
-        <li>
-          <NavLink
-            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
-            to="/"
-          >
-            <FaFire />
+        {
+          sidebarList.map(side => (
+            <li key={side.id}>
+              <NavLink
+                className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+                 to={side.path}
+              >
+                { side.icon }
 
-            <span>热门榜</span>
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink
-            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
-            to="/"
-          >
-            <FaFolderPlus />
-
-            <span>新品发布</span>
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink
-            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
-            to="/"
-          >
-            <FaCalendarAlt />
-
-            <span>即将推出</span>
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink
-            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
-            to="/"
-          >
-            <FaRegHeart />
-
-            <span>收藏夹</span>
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink
-            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
-            to="/"
-          >
-            <FaRegClock />
-
-            <span>稍后观看</span>
-          </NavLink>
-        </li>
+                <span>{ side.name }</span>
+              </NavLink>
+            </li>
+          ))
+        }
       </ul>
     </aside>
   )
