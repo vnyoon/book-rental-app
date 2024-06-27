f13a81d7b1f8f36108fc310c19c30eb4e2da15fc
vnyoon <vnyoon@163.com>
2024-06-27 17:42:19 +0800

    Sidebar layout and style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,74 @@
 import React, { memo } from 'react'
+import { NavLink } from "react-router-dom";
+
+import { 
+  FaFire, 
+  FaFolderPlus, 
+  FaCalendarAlt, 
+  FaRegHeart,
+  FaRegClock, 
+} from "react-icons/fa";
 
 const Sidebar = memo(() => {
   return (
-    <div>Sidebar</div>
+    <aside className="bg-orange-500">
+      <ul className="space-y-2">
+        <li>
+          <NavLink
+            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+            to="/"
+          >
+            <FaFire />
+
+            <span>热门榜</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+            to="/"
+          >
+            <FaFolderPlus />
+
+            <span>新品发布</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+            to="/"
+          >
+            <FaCalendarAlt />
+
+            <span>即将推出</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+            to="/"
+          >
+            <FaRegHeart />
+
+            <span>收藏夹</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            className={({ isActive }) => `flex items-center space-x-2 px-5 py-3.5 rounded-lg ${isActive && 'bg-primary text-white'}`}
+            to="/"
+          >
+            <FaRegClock />
+
+            <span>稍后观看</span>
+          </NavLink>
+        </li>
+      </ul>
+    </aside>
   )
 })
 
